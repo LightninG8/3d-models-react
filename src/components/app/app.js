0e5ad8571903bf1772981debcb693e9db8fd5d0b
Алексей <tov.kessler@gmail.com>
2020-12-19 12:48:25 +0800

    Add: change normal map, settings

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,22 +2,22 @@
 import React, {useEffect, useState} from "react";
 
 import './app.css';
-// import model from "../../models/monkey/monkey.gltf";
-// import model from "../../models/peoples/women.glb";
+
+import settingsIcon from "../../components/app/icon.png";
+
+
 import model from "../../models/peoples/womandress/womendress.gltf";
-// import model from "../../models/DamagedHelmet/DamagedHelmet.gltf";
 
-// import texture0 from "../../models/monkey/monkey_img0.png";
-// import texture1 from "../../models/monkey/Untitled.001.png";
-// import texture2 from "../../models/monkey/Untitled.002.png";
-// import texture4 from "../../models/monkey/Untitled.004.png";
-
+import textureNone from "../../models/peoples/womandress/noneTexture.jpg";
 import texture1col from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_COL_VAR1_1K.jpg";
 import texture2col from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_COL_VAR1_2K.jpeg";
 import texture3col from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_COL_VAR1_3K.jpeg";
-// import texture1ocl from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_COL_VAR1_4K.jpeg";
+
 import texture1refl from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_REFL_1K.jpg";
 import texture2refl from "../../models/peoples/womandress/womendress_img2.png";
+
+import texture1nor from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_NRM_1K.jpg";
+import texture2nor from "../../models/peoples/womandress/FabricUpholsteryMidCenturyPebbles001_NRM_2K.jpeg";
 
 function App() {
 	const [metalness, setMetalness] = useState(0);
@@ -37,6 +37,9 @@ function App() {
 			const applyPBRTexture = (channel, event) => {
 				material.pbrMetallicRoughness[channel].texture.source.setURI(event.target.value);
 			};
+			const applyTexture = (channel, event) => {
+				material[channel].texture.source.setURI(event.target.value);
+			};
 			// const applyTexture = (channel, event) => {
 			// 	material[channel].texture.source.setURI(event.target.value);
 			// };
@@ -49,6 +52,10 @@ function App() {
 			document.querySelector('#metallicRoughness').addEventListener('input', (event) => {
 				applyPBRTexture('metallicRoughnessTexture', event);
 			});
+			document.querySelector('#normal').addEventListener('input', (event) => {
+				applyTexture('normalTexture', event);
+			});
+
 			// document.querySelector('#occlusion').addEventListener('input', (event) => {
 			// 	applyTexture('occlusionTexture', event);
 			// });
@@ -78,10 +85,19 @@ function App() {
 		return event.target.value;
 	}
 
+	function toggleSettingsVisible() {
+		const settings = document.querySelector("#controls");
+		settings.hidden = !settings.hidden;
 
+		if (!settings.hidden) {
+			settings.style.display = "flex";
+		} else {
+			settings.style.display = "none";
+		}
+	}
 	return (
 		<div className="app" >
-			<div className="app__body container">
+			<div className="app__body">
 				<model-viewer 
 					id="lantern" 
 					class="model-viewer" 
@@ -90,41 +106,47 @@ function App() {
 					exposure="1" 
 					skybox-image="https://modelviewer.dev/shared-assets/environments/spruit_sunrise_1k_HDR.hdr"
 					>
-						
+					<div id="controlsBurger" onClick={toggleSettingsVisible}>
+						<img src={settingsIcon} alt="" className="controlsBurger__icon"/>
+					</div>
+					<div id="controls">
+						<div>
+							<p>Diffuse</p>
+							<select id="diffuse">
+								<option value={texture1col}>Texture 1</option>
+								<option value={texture2col}>Texture 2</option>
+								<option value={texture3col}>Texture 3</option>
+								<option value={textureNone}>None</option>
+							</select>
+						</div>
+						<div>
+							<p>Metallic-roughness</p>
+							<select id="metallicRoughness">
+								<option value={texture1refl}>Texture 1</option>
+								<option value={texture2refl}>Texture 2</option>
+								<option value={textureNone}>None</option>
+							</select>
+						</div>
+						<div>
+							<p>Normal map</p>
+							<select id="normal">
+								<option value={texture1nor}>Texture 1</option>
+								<option value={texture2nor}>Texture 2</option>
+								<option value={textureNone}>None</option>
+							</select>
+						</div>
+						<div>
+							<p>Metalness: <span id="metalness-value">{metalness}</span></p>
+							<input id="metalness" type="range" min="0" max="1" step="0.01" value={metalness} onChange={(event) => setMetalness(onChangeRange(event))}></input>
+						</div>
+						<div>
+							<p>Roughness: <span id="roughness-value">{roughness}</span></p>
+							<input id="roughness" type="range" min="0" max="1" step="0.01" value={roughness} onChange={(event) => setRoughness(onChangeRange(event))}></input>
+						</div>
+					</div>
 	
 				</model-viewer>
-				<div id="controls">
-					<div>
-						<p>Diffuse</p>
-						<select id="diffuse">
-							<option value={texture1col}>Texture 1</option>
-							<option value={texture2col}>Texture 2</option>
-							<option value={texture3col}>Texture 3</option>
-						</select>
-					</div>
-					<div>
-						<p>Metallic-roughness</p>
-						<select id="metallicRoughness">
-							<option value={texture1refl}>Texture 1</option>
-							<option value={texture2refl}>Texture 2</option>
-						</select>
-					</div>
-					{/* <div>
-						<p>Occlusion</p>
-						<select id="occlusion">
-							<option>Texture 1</option>
-							<option value={texture1ocl}>Texture 1</option>
-						</select>
-					</div> */}
-					<div>
-						<p>Metalness: <span id="metalness-value">{metalness}</span></p>
-						<input id="metalness" type="range" min="0" max="1" step="0.01" value={metalness} onChange={(event) => setMetalness(onChangeRange(event))}></input>
-					</div>
-					<div>
-						<p>Roughness: <span id="roughness-value">{roughness}</span></p>
-						<input id="roughness" type="range" min="0" max="1" step="0.01" value={roughness} onChange={(event) => setRoughness(onChangeRange(event))}></input>
-					</div>
-				</div>
+				
 			</div>
 		</div>
 	);
